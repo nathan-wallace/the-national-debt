e39c3f8ad88197eb82ff2348227b5b443fa2c600
Nathan Wallace <nathan.s.wallace@gmail.com>
2025-03-10 11:17:58 -0400

    build update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
-// webpack.config.js
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // Webpack will override this with --mode production in the build script
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.[contenthash].js', // Add contenthash for cache busting
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    clean: true, // Automatically clean the dist folder before each build
   },
   module: {
     rules: [
@@ -24,34 +21,5 @@ module.exports = {
     hot: true,
     open: true,
     historyApiFallback: true,
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public/images', to: 'images' },
-      ],
-    }),
-  ],
-  optimization: { // Add this section
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({ // Explicitly use Terser for JS minification
-        extractComments: false, // Don’t extract comments to a separate file
-        terserOptions: {
-          compress: { drop_console: true }, // Remove console.logs in production
-          mangle: true, // Shorten variable names
-        },
-      }),
-    ],
-    splitChunks: { // Split vendor code (e.g., D3) into a separate chunk
-      chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
   },
 };
